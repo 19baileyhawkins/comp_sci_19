90930525bf9e0d15424efe47b6802b2956c2d449
Bailey <47828976+19baileyhawkins@users.noreply.github.com>
2019-02-27 14:12:59 -0500

    Update Tkinter.py

--- Tkinter.py
+++ Tkinter.py
@@ -3,12 +3,6 @@ import turtle
 t = turtle.Pen()
 t.speed(100)
 
-class Buttons:
-        def __init__(self, master):
-                #create a frame in the main window "master"
-                frame = Frame(master)
-                frame.pack()
-
 def draw_a_left_spiral():
     for x in range (200):
         t.forward(x)
@@ -43,66 +37,69 @@ root.title("~~~Turtle Drawing~~~")
 frame = tk.Frame(root)
 frame.pack()
 
+colorframe = tk.Frame(root)
+colorframe.pack()
+shapeframe = tk.Frame(root)
+shapeframe.pack()
+bottomframe = tk.Frame(root)
+bottomframe.pack(side=tk.BOTTOM)
+
+l= tk.Label(root,
+        text="Pick a color and a shape to draw")
+l.pack()
+
 ###color buttons
-self.blue_button = tk.Button(colorframe, 
+blue_button = tk.Button(colorframe, 
                    text="Blue", 
                    fg="blue",
                    command=make_it_blue)
-#blue_button.pack(side=tk.LEFT)
+blue_button.pack(side=tk.LEFT)
 
-self.red_button = tk.Button(colorframe, 
+red_button = tk.Button(colorframe, 
                    text="Red", 
                    fg="red",
                    command=make_it_red)
-#red_button.pack(side=tk.LEFT)
+red_button.pack(side=tk.LEFT)
 
-self.yellow_button = tk.Button(colorframe, 
+yellow_button = tk.Button(colorframe, 
                    text="Yellow", 
                    fg="yellow",
                    command=make_it_yellow)
-#yellow_button.pack(side=tk.LEFT)
+yellow_button.pack(side=tk.LEFT)
 
 ###shape buttons
-self.left_spiral_button = tk.Button(shapeframe, 
+left_spiral_button = tk.Button(shapeframe, 
                    text="Left Spiral", 
                    fg="black",
                    command=draw_a_left_spiral)
-#left_spiral_button.pack(side=tk.LEFT)
+left_spiral_button.pack(side=tk.LEFT)
 
-self.right_spiral_button = tk.Button(shapeframe, 
+right_spiral_button = tk.Button(shapeframe, 
                    text="Right Spiral", 
                    fg="black",
                    command=draw_a_right_spiral)
-#right_spiral_button.pack(side=tk.LEFT)
+right_spiral_button.pack(side=tk.LEFT)
 
-self.left_tri_button = tk.Button(frame, 
+left_tri_button = tk.Button(shapeframe, 
                    text="Left Triangle", 
                    fg="black",
                    command=draw_a_left_tri)
-#left_tri_button.pack(side=tk.LEFT)
+left_tri_button.pack(side=tk.LEFT)
 
-self.right_tri_button = tk.Button(shapeframe, 
+right_tri_button = tk.Button(shapeframe, 
                    text="Right Triangle", 
                    fg="black",
                    command=draw_a_right_tri)
-#right_tri_button.pack(side=tk.LEFT)
+right_tri_button.pack(side=tk.LEFT)
 
 ###the quit button
-self.quit_button = tk.Button(bottomframe,
+quit_button = tk.Button(bottomframe,
                    text="Quit",
                    command=quit)
-self.quit_button.pack(side=tk.LEFT)
+quit_button.pack(side=tk.LEFT)
 
-###the frames
-colorframe = Frame(root)
-colorframe.pack()
-shapeframe = Frame(root)
-shapeframe.pack()
-bottomframe =Frame(root)
-bottomframe.pack(side=BOTTOM)
 
 root.mainloop()
+
     
-    
-                    
     
